a81fb4f77f38c919cb76da707fd5ac876211ae20
gift56 <efeasiughu@gmail.com>
2023-04-29 07:50:08 -0700

    imported Icons and Imgaes

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,4 +2,68 @@ import Logo from "./img/logo.svg";
 import UserProfile from "./img/user_profile.png";
 import LoginImg from "./img/loginImg.png";
 
-export { Logo, UserProfile, LoginImg };
+// Icons imports
+import BriefCaseIcon from "./icons/briefcase.svg";
+import HomeIcon from "./icons/home.svg";
+import UsersIcon from "./icons/user-friends.svg";
+import GuarntorIcon from "./icons/users.svg";
+import LoanIcon from "./icons/sack.svg";
+import DecisionIcon from "./icons/handshake.svg";
+import SavingsIcon from "./icons/piggy-bank.svg";
+import RequestIcon from "./icons/loanSave.svg";
+import WaitliastIcon from "./icons/user-check.svg";
+import KarmaIcon from "./icons/user-times.svg";
+import ProductSavingIcon from "./icons/saving.svg";
+import FeesIcon from "./icons/coins-solid.svg";
+import TransactionIcon from "./icons/transation.svg";
+import ServiceIcon from "./icons/galaxy.svg";
+import AccountServiceIcon from "./icons/user-cog.svg";
+import SettlementIcon from "./icons/scroll.svg";
+import ReportsIcon from "./icons/chart-bar.svg";
+import PerformanceIcon from "./icons/sliders-h.svg";
+import PricingIcon from "./icons/badge-percent.svg";
+import AuditIcon from "./icons/clipboard-list.svg";
+import dropdownIcon from "./icons/dropDown.svg";
+import selectIcon from "./icons/selectIcon.svg";
+import card1Icon from "./icons/usersIcon.svg";
+import card2Icon from "./icons/activeUserIcon.svg";
+import card3Icon from "./icons/loanUser.svg";
+import card4Icon from "./icons/savingUser.svg";
+import notifyIcon from "./icons/notify.svg";
+import Avatar from "./icons/avatar.svg";
+import BackIcon from "./icons/backarrow.svg";
+
+export {
+  Logo,
+  UserProfile,
+  LoginImg,
+  Avatar,
+  BackIcon,
+  notifyIcon,
+  card1Icon,
+  card2Icon,
+  card3Icon,
+  card4Icon,
+  selectIcon,
+  dropdownIcon,
+  AuditIcon,
+  BriefCaseIcon,
+  HomeIcon,
+  UsersIcon,
+  GuarntorIcon,
+  LoanIcon,
+  DecisionIcon,
+  SavingsIcon,
+  ReportsIcon,
+  RequestIcon,
+  PerformanceIcon,
+  WaitliastIcon,
+  KarmaIcon,
+  PricingIcon,
+  ProductSavingIcon,
+  FeesIcon,
+  TransactionIcon,
+  ServiceIcon,
+  AccountServiceIcon,
+  SettlementIcon,
+};
