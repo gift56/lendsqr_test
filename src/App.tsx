c3a2fcb126a1c9b85fd731c6f784c3e0e12fabd7
gift56 <efeasiughu@gmail.com>
2023-04-29 10:03:08 -0700

    working on dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import Dashboardlayout from "./layout/Dashboardlayout";
+import DashboardPages from './pages/DashboardPages'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route element={<Dashboardlayout />}>
+        <Route
+          path="/dashboard/*"
+          element={<DashboardPages />}
+        />
+      </Route>
     </Routes>
   );
 };
