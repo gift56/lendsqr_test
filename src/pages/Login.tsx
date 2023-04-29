bf23a3d23eb7f31f23718d973bee4984376eab3f
gift56 <efeasiughu@gmail.com>
2023-04-29 08:44:46 -0700

    working on login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,22 @@
 import styles from "../styles/login.module.scss";
+import { LoginImg, Logo } from "../assets";
 
 const Login = () => {
-  return <div className={styles.warpper}>
-    
-  </div>;
+  return (
+    <div className={styles.warpper}>
+      <div className={styles.leftSide}>
+        <div className={styles.logo}>
+          <img src={Logo} alt="lendsqr_logo" />
+        </div>
+        <div className={styles.mainImg}>
+          <div className={styles.loginImg}>
+            <img src={LoginImg} alt="login_img" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.rightSide}></div>
+    </div>
+  );
 };
 
 export default Login;
