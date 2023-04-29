eb878415c016d8b6952e552c7a6079ddaf3d81b0
gift56 <efeasiughu@gmail.com>
2023-04-29 10:37:19 -0700

    working on dashboard page

--- src/components/DashboardContents/DashboardHeader/DashboardHeader.tsx
+++ src/components/DashboardContents/DashboardHeader/DashboardHeader.tsx
@@ -1,7 +1,32 @@
 import style from "../../../styles/dashoboard.module.scss";
+import { Logo, notifyIcon } from "../../../assets";
+import { CustomizeInput } from "../..";
+import { GrSearch } from "react-icons/gr";
+import { Link } from "react-router-dom";
 
 const DashboardHeader = () => {
-  return <header className={style.header}>DashboardHeader</header>;
+  return (
+    <header className={style.header}>
+      <div className={style.logoArea}>
+        <div className={style.logo}>
+          <img src={Logo} alt="lendsqr_logo" />
+        </div>
+        <div className={style.searchCon}>
+          <CustomizeInput type="text" placeholder="Search for anything" />
+          <span>
+            <GrSearch />
+          </span>
+        </div>
+      </div>
+      <div className={style.dropDownArea}>
+        <Link to="/dashboard">Docs</Link>
+        <img src={notifyIcon} alt="/" />
+        <div className={style.userDrop}>
+            
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default DashboardHeader;
