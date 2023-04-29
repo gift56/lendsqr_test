93fdbb264f3515fd9d38eafba89b290a575f46fe
gift56 <efeasiughu@gmail.com>
2023-04-29 07:29:48 -0700

    working on imports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,5 @@
 import Logo from "./img/logo.svg";
+import UserProfile from "./img/user_profile.png";
+import LoginImg from "./img/loginImg.png";
 
-export { Logo };
+export { Logo, UserProfile, LoginImg };
