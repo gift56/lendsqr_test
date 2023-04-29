434b15ce85a410e04de3014a3a784af50334a0f5
gift56 <efeasiughu@gmail.com>
2023-04-29 10:15:02 -0700

    working on dashboard

--- src/layout/Dashboardlayout.tsx
+++ src/layout/Dashboardlayout.tsx
@@ -1,9 +1,21 @@
+import { DashboardHeader, DashboardSidebar } from "../components";
+import { Outlet } from "react-router-dom";
 import style from "../styles/layout.module.scss";
 
 const Dashboardlayout = () => {
-  return <main className={style.mainWrapper}>
-    h
-  </main>;
+  return (
+    <main className={style.mainWrapper}>
+      <DashboardHeader />
+      <div className={style.wrapperContent}>
+        <div className={style.sidebar}>
+          <DashboardSidebar />
+        </div>
+        <div className={style.outlet}>
+          <Outlet />
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Dashboardlayout;
