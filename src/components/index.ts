b3750b12241a3d64517cf2bda6b44bb247fecd9e
gift56 <efeasiughu@gmail.com>
2023-04-29 13:54:59 -0700

    working on dashboard page

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,13 @@ import CustomizeInput from "./Inputs/CustomizeInput";
 import DashboardSidebar from "./DashboardContents/DashboardSidebar/DashboardSidebar";
 import DashboardHeader from "./DashboardContents/DashboardHeader/DashboardHeader";
 import Card from "./Card/Card";
+import Pagination from "./Pagination/Pagination";
 
-export { Button, CustomizeInput, DashboardSidebar, DashboardHeader, Card };
+export {
+  Button,
+  CustomizeInput,
+  DashboardSidebar,
+  DashboardHeader,
+  Card,
+  Pagination,
+};
--- src/pages/DashboardPages/Dashboard.tsx
+++ src/pages/DashboardPages/Dashboard.tsx
@@ -1,4 +1,5 @@
-import { Card } from "../../components";
+import { selectIcon } from "../../assets";
+import { Card, Pagination } from "../../components";
 import UserTable from "../../components/UserTable/UserTable";
 import style from "../../styles/dashoboard.module.scss";
 import { cardData } from "../../utils/cardData";
@@ -14,6 +15,19 @@ const Dashboard = () => {
       </div>
       <div className={style.tableCon}>
         <UserTable />
+        <div className={style.paginate}>
+          <div className={style.showing}>
+            <span>Showing</span>
+            <div className={style.filterCon}>
+              <span>100</span>
+              <img src={selectIcon} alt="/" />
+            </div>
+            <span>out of 100</span>
+          </div>
+          <div>
+            <Pagination />
+          </div>
+        </div>
       </div>
     </div>
   );
