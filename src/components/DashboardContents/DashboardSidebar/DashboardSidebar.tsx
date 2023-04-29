3dd6728b0f33e59baa92c0391411420e3b6789b3
gift56 <efeasiughu@gmail.com>
2023-04-29 11:50:34 -0700

    working on dashboard sidebar

--- src/components/DashboardContents/DashboardSidebar/DashboardSidebar.tsx
+++ src/components/DashboardContents/DashboardSidebar/DashboardSidebar.tsx
@@ -1,9 +1,23 @@
+import { BriefCaseIcon, HomeIcon, selectIcon } from "../../../assets";
 import style from "../../../styles/dashoboard.module.scss";
+import { NavLink } from "react-router-dom";
 
 const DashboardSidebar = () => {
-  return <div className={style.sidebarCon}>
-    
-  </div>;
+  return (
+    <div className={style.sidebarCon}>
+      <div className={style.switchLink}>
+        <div className={style.link}>
+          <img src={BriefCaseIcon} alt="/" />
+          <span>Switch Organization</span>
+          <img src={selectIcon} alt="/" />
+        </div>
+        <NavLink to="/dashboard" className={`${style.link} sidebar`}>
+          <img src={HomeIcon} alt="/" />
+          <span>Dashboard</span>
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardSidebar;
