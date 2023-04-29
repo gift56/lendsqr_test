918dbfcffe535bf13b6a4680a1c52678c829d374
gift56 <efeasiughu@gmail.com>
2023-04-29 08:08:34 -0700

    working on login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  );
 };
 
 export default App;
