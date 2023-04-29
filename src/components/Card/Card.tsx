30946a6bd9a0d2b2b32e022139ff956679bc7b28
gift56 <efeasiughu@gmail.com>
2023-04-29 13:25:58 -0700

    working on dashboard page

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,21 @@
-const Card = ({ data }: any) => {
-  return <div>Card</div>;
+import style from "../../styles/dashoboard.module.scss";
+
+interface IProp {
+  data: {
+    img: string;
+    text: string;
+    totalNo: string;
+  };
+}
+
+const Card = ({ data }: IProp) => {
+  return (
+    <div className={style.card}>
+      <img src={data.img} alt="/" />
+      <h5>{data.text}</h5>
+      <h3>{data.totalNo}</h3>
+    </div>
+  );
 };
 
 export default Card;
