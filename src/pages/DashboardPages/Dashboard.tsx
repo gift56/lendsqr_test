7a75133a4953020896023584d04dbbb0f9ce2667
gift56 <efeasiughu@gmail.com>
2023-04-29 13:07:01 -0700

    working on dashboard page

--- src/pages/DashboardPages/Dashboard.tsx
+++ src/pages/DashboardPages/Dashboard.tsx
@@ -1,7 +1,11 @@
 import style from "../../styles/dashoboard.module.scss";
 
 const Dashboard = () => {
-  return <div className={style.mainDashboard}>Dashboard</div>;
+  return (
+    <div className={style.mainDashboard}>
+      <h2>Dashboard</h2>
+    </div>
+  );
 };
 
 export default Dashboard;
