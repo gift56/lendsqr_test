bff6dc81f4b9c144fd35130100807faa10f31f9e
gift56 <efeasiughu@gmail.com>
2023-04-29 15:50:55 -0700

    added pagination

--- src/components/UserTable/MobileTable/MobileTable.tsx
+++ src/components/UserTable/MobileTable/MobileTable.tsx
@@ -23,40 +23,18 @@ const MobileTable = ({ data, text, show, className, to }: IProp) => {
   };
 
   return (
-    <div
-      className={`w-full mx-auto xl:hidden flex items-start flex-col gap-3 justify-start ${className}`}
-    >
-      {show === true && (
-        <h2 className="text-darkColor font-semibold uppercase">{text}</h2>
-      )}
+    <div className={`${style.table} ${className}`}>
+      {show === true && <h2 className={style.h2}>{text}</h2>}
       <div className="w-full bg-white rounded-lg flex items-start justify-start flex-col shadow-smallShadow border">
         {data.slice(0, 8).map((item: any, i: any) => (
-          <Link
-            to={`${to}${item.id}`}
-            key={i}
-            className="w-full border-b  p-4 text-sm border-[#E6E6E6] last:border-none flex items-start justify-between"
-          >
-            <div className="flex items-start justify-start flex-col gap-1">
-              <div className="flex items-start justify-start flex-col gap-1">
-                <h2 className="text-darkColor text-sm font-medium phone:text-base">
-                  {item.userName}
-                </h2>
-                <p className="text-xs text-[#5D6771] font-medium">
-                  {item.email}
-                </p>
-              </div>
+          <Link to={`${to}${item.id}`} key={i} className={style.contentWrapper}>
+            <div className={style.contents}>
+              <h2 className={style.content}>{item.userName}</h2>
+              <p className={style.content}>{item.email}</p>
             </div>
-            <div className="flex items-end justify-end flex-col gap-2">
-              <p className="text-darkColor text-sm font-medium phone:text-base">
-                {item.phone}
-              </p>
-              <p
-                className={`${statusColor(
-                  item.status
-                )} text-xs font-semibold capitalize w-fit py-[4px] px-[10px] rounded-[100px]`}
-              >
-                {item.status}
-              </p>
+            <div className={style.contents}>
+              <p className={style.content}>{item.phone}</p>
+              <p className={`${statusColor(item.status)}`}>{item.status}</p>
             </div>
           </Link>
         ))}
