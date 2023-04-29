242bb1d5e0f769032a5cecedadb9c5c8aad8f2b9
gift56 <efeasiughu@gmail.com>
2023-04-29 16:11:31 -0700

    working on dashboard users page

--- src/pages/DashboardPages/index.tsx
+++ src/pages/DashboardPages/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard";
+import DashboardUser from "./DashboardUser";
 
 const index = () => {
   return (
     <Routes>
       <Route path="/home" element={<Dashboard />} />
+      <Route path="/users" element={<DashboardUser />} />
     </Routes>
   );
 };
