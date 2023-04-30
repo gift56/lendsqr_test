982a041d76dcf0e3f467014a5c49964e682290b8
gift56 <efeasiughu@gmail.com>
2023-04-30 06:51:17 -0700

    fetching user data

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,2 +1,16 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+
+const useStore = (set: any) => ({
+  allUsers: [],
+  setAllUsers: (allUsers: any) =>
+    set((state: any) => ({ ...state, allUsers: allUsers })),
+});
+
+const useUserStore = create(
+    persist(useStore, {
+      name: "store",
+    })
+  );
+  
+  export default useUserStore;
