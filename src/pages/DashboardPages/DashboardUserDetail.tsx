23db307d7587e34251c80d41e5f5756e3d8c5b4f
gift56 <efeasiughu@gmail.com>
2023-04-29 18:20:29 -0700

    working on user details page

--- src/pages/DashboardPages/DashboardUserDetail.tsx
+++ src/pages/DashboardPages/DashboardUserDetail.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import style from "../../styles/dashoboard.module.scss";
+import { BackIcon } from "../../assets";
+import { Link } from "react-router-dom";
 
 const DashboardUserDetail = () => {
-  return <div>DashboardUserDetail</div>;
+  const [tab, setTab] = useState("General");
+
+  return (
+    <div className={style.mainDashboard}>
+      <Link to="/dashboard/users" className={style.back}>
+        <img src={BackIcon} alt="/" />
+        <span>Back to Users</span>
+      </Link>
+    </div>
+  );
 };
 
 export default DashboardUserDetail;
