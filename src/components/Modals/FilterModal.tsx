cb3988b2450c1a2420168c30519195924722e85c
gift56 <efeasiughu@gmail.com>
2023-04-29 17:04:54 -0700

    working on filter  modal

--- src/components/Modals/FilterModal.tsx
+++ src/components/Modals/FilterModal.tsx
@@ -48,6 +48,69 @@ const FilterModal = ({ show, setShow }: any) => {
         className="mainInput"
         placeholder="Username"
       />
+      <CustomizeInput
+        showLabel={false}
+        label="Email"
+        htmlFor="email"
+        id="email"
+        type="text"
+        // value={values.email}
+        // onChange={handleChange}
+        // onBlur={handleBlur}
+        // error={getError("email")}
+        name="email"
+        labelClassName={style.label}
+        className="mainInput"
+        placeholder="email"
+      />
+      <CustomizeInput
+        showLabel={false}
+        label="Date"
+        htmlFor="date"
+        id="date"
+        type="date"
+        // value={values.date}
+        // onChange={handleChange}
+        // onBlur={handleBlur}
+        // error={getError("date")}
+        name="date"
+        labelClassName={style.label}
+        className="mainInput"
+        placeholder="date"
+      />{" "}
+      <CustomizeInput
+        showLabel={false}
+        label="Phone Number"
+        htmlFor="phone"
+        id="phone"
+        type="text"
+        // value={values.phone}
+        // onChange={handleChange}
+        // onBlur={handleBlur}
+        // error={getError("phone")}
+        name="phone"
+        labelClassName={style.label}
+        className="mainInput"
+        placeholder="Phone Number"
+      />
+      <CustormizeSelect
+        showLabel={false}
+        label="Status"
+        // value={values.status}
+        // onChange={handleChange}
+        // onBlur={handleBlur}
+        // error={getError("status")}
+        htmlFor="status"
+        name="status"
+        labelClassName={style.label}
+        className="mainInput"
+      >
+        {filterOptions.map((option: any, i: any) => (
+          <option id="status" value={option.value} key={i}>
+            {option.text}
+          </option>
+        ))}
+      </CustormizeSelect>
     </div>
   );
 };
