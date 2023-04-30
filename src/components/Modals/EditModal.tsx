0e9d43ee7153d076738eb6dc359b146cafdadde1
gift56 <efeasiughu@gmail.com>
2023-04-29 17:53:26 -0700

    done working on detials modal

--- src/components/Modals/EditModal.tsx
+++ src/components/Modals/EditModal.tsx
@@ -1,7 +1,9 @@
 import style from "../../styles/dashoboard.module.scss";
 
 const EditModal = ({ show, setShow }: any) => {
-  return <div className={style.editContainer}>EditModla</div>;
+  return <div className={style.editContainer}>
+    
+  </div>;
 };
 
 export default EditModal;
