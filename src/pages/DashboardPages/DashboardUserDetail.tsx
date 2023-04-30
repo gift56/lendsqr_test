a6e58b5fe447e8bab3164cc90963dabe53f0c92e
gift56 <efeasiughu@gmail.com>
2023-04-29 18:24:48 -0700

    working on user details page

--- src/pages/DashboardPages/DashboardUserDetail.tsx
+++ src/pages/DashboardPages/DashboardUserDetail.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import style from "../../styles/dashoboard.module.scss";
 import { BackIcon } from "../../assets";
 import { Link } from "react-router-dom";
+import { Button } from "../../components";
 
 const DashboardUserDetail = () => {
   const [tab, setTab] = useState("General");
@@ -12,6 +13,25 @@ const DashboardUserDetail = () => {
         <img src={BackIcon} alt="/" />
         <span>Back to Users</span>
       </Link>
+      <div className={style.headingStyle}>
+        <h2>User Details</h2>
+        <div className={style.buttons}>
+          <Button
+            disabled={false}
+            type="button"
+            text="Blacklist User"
+            className={style.blacklist}
+            onClick={() => {}}
+          />
+          <Button
+            disabled={false}
+            type="button"
+            text="Blacklist User"
+            className={style.activate}
+            onClick={() => {}}
+          />
+        </div>
+      </div>
     </div>
   );
 };
